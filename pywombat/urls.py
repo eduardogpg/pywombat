5f11ea0436d2d7440a3682af4bfce3e177d2499d
eduardogpg <eduardo78d@gmail.com>
2021-09-07 17:02:26 -0500

    Archivos staticos

--- pywombat/urls.py
+++ pywombat/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from .views import index
 
 urlpatterns = [
     path('', index, name='index'),
     path('admin/', admin.site.urls),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
